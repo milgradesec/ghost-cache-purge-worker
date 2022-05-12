9ce3460474c869fb3123a9e064c257c941751ab5
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-05-12 14:52:25 +0200

    Migrate to module worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request, env, context) {
+    return handleRequest(request, env, context)
+  },
+};
 
-async function handleRequest(request) {
+async function handleRequest(request, env, context) {
   const { headers } = request
   const contentType = headers.get("content-type") || ""
   const url = new URL(request.url);
@@ -12,12 +14,12 @@ async function handleRequest(request) {
   }
 
   if (url.pathname.startsWith("/postPublished")) {
-    const resp = await purgeURL(GHOST_URL)
+    const resp = await purgeURL(env.GHOST_URL)
     if (!resp.ok) {
       return new Response(resp.statusText, { status: resp.status })
     }
 
-    console.log("Purged: " + GHOST_URL)
+    console.log("Purged: " + env.GHOST_URL)
     return new Response("OK", { status: 200 })
   }
 
@@ -27,9 +29,9 @@ async function handleRequest(request) {
     }
 
     const json = await parseJsonBody(request)
-    const postURL = GHOST_URL + json.post.current.slug + "/"
+    const postURL = env.GHOST_URL + json.post.current.slug + "/"
 
-    const resp = await purgeURL(postURL)
+    const resp = await purgeURL(postURL, env)
     if (!resp.ok) {
       return new Response(resp.statusText, { status: resp.status })
     }
@@ -48,16 +50,16 @@ async function handleRequest(request) {
  * @param {String} url Url to purge from the cache
  * @returns {Promise<Response>} response from Cloudflare API
  */
-async function purgeURL(url) {
+async function purgeURL(url, env) {
   const requestInit = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${CF_API_TOKEN}`
+      "Authorization": `Bearer ${env.CF_API_TOKEN}`
     },
     body: `{"files":["${url}"]}`
   }
-  return await fetch(`https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache`, requestInit)
+  return await fetch(`https://api.cloudflare.com/client/v4/zones/${env.CF_ZONE_ID}/purge_cache`, requestInit)
 }
 
 
